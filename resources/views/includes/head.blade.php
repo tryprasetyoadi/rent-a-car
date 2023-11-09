62798646e8ed2f4c30daa408ea45f817f1ae29ce
prasetyo <tryprasetyoadi2420@gmail.com>
2023-11-09 18:02:40 +0700

    ico

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/fontawesome/css/all.css') }}">
+<link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
 
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
